3e96c09d1f5bc80e528951d45b0dd202c914bb1c
Tiegbe Bamba <bvmbinoworldshop@gmail.com>
2026-03-17 07:26:18 +0100

    Rename index.html to FormationPro.html and update sw.js for deployment

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'formation-pro-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
